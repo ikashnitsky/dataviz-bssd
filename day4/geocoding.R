14a41427cc7a37eee5a26da0c505fd5fd49a1798
ikashnitsky <ilya.kashnitsky@gmail.com>
2022-07-22 08:23:51 +0200

    Update geocoding.R

--- day4/geocoding.R
+++ day4/geocoding.R
@@ -19,54 +19,60 @@ library(gsheet)
 raw <- gsheet2tbl("https://docs.google.com/spreadsheets/d/1YlfLQc_aOOiTqaSGu5TI70OQy1ewTa_Ti0qAEOEcy58")
 
 df <- raw %>% 
-  janitor::clean_names() %>% 
-  drop_na() %>% 
-  mutate(text_to_geocode = paste(city_settlement, country, sep = ", "))
+    janitor::clean_names() %>% 
+    drop_na() %>% 
+    mutate(text_to_geocode = paste(city_settlement, country, sep = ", "))
 
 # now geocode
 # there is a new brilliant package tidygeocoder
 library(tidygeocoder)
 
 df_geocoded <- df %>% 
-  geocode(text_to_geocode, method = "osm")
+    geocode(text_to_geocode, method = "osm")
 
 # convert coordinates to an sf object
 df_plot <- df_geocoded %>% 
-  drop_na() %>% 
-  st_as_sf(
-    coords = c("long", "lat"),
-    crs = 4326
-  )
+    drop_na() %>% 
+    st_as_sf(
+        coords = c("long", "lat"),
+        crs = 4326
+    )
 
 
 # get world map outline (you might need to install the package)
 world_outline <- spData::world %>% 
-  st_as_sf()
+    st_as_sf()
 
 # let's use a fancy projection
 world_outline_robinson <- world_outline %>% 
-  st_transform(crs = "ESRI:54030")
+    st_transform(crs = "ESRI:54030")
 
 country_borders <- world_outline_robinson %>% 
-  rmapshaper::ms_innerlines()
+    rmapshaper::ms_innerlines()
 
 
 # map!
 world_outline_robinson %>% 
-  filter(!iso_a2 == "AQ") %>% 
-  ggplot()+
-  geom_sf(fill = "#673862", color = NA)+
-  geom_sf(data = country_borders, size = .25, color = "#BD92B7FF")+
-  geom_sf(data = df_plot, color = "#FDD15E", size = 1.5, shape = 16)+
-  dark_theme_minimal()+
-  labs(
-    title = "Birth places of the participants",
-    subtitle = "BSSD Dataviz course at CED, July 2021",
-    caption = "@ikashnitsky"
-  )+
-  theme(
-    plot.background = element_rect(fill = "#222222", color = NA),
-    axis.text = element_blank()
-  )
+    filter(!iso_a2 == "AQ") %>% 
+    ggplot()+
+    geom_sf(fill = "#673862", color = NA)+
+    geom_sf(data = country_borders, size = .25, color = "#BD92B7FF")+
+    geom_sf(
+        data = df_plot, fill = "#FDD15E", 
+        color = "#FDD15E" %>% prismatic::clr_darken(),
+        size = 1.5, shape = 21
+    )+
+    coord_sf(datum = NA)+
+    dark_theme_minimal(base_family = "Atkinson Hyperlegible")+
+    labs(
+        title = "Birth places of the participants",
+        subtitle = "BSSD Dataviz course at CED, July 2022",
+        caption = "@ikashnitsky"
+    )+
+    theme(
+        plot.background = element_rect(fill = "#15202b", color = NA),
+        axis.text = element_blank(),
+        plot.title = element_text(face = 2, size = 18)
+    )
 
-ggsave("out/map-birth-places.png", width = 5, height = 3)  
+ggsave("out/map-birth-places.png", width = 5.33, height = 3)  
