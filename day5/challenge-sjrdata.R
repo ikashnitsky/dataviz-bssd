45093372faec9804a5eee27842cfbbfa270800e7
ikashnitsky <ilya.kashnitsky@gmail.com>
2022-07-22 09:00:16 +0200

    day 5

---
+++ day5/challenge-sjrdata.R
@@ -0,0 +1,24 @@
+#===============================================================================
+# 2022-07-22 -- BSSD dataviz
+# Dataviz challenge -- sjrdata
+# Ilya Kashnitsky, ilya.kashnitsky@gmail.com
+#===============================================================================
+
+# SCImago Journal & Country Rank provides valuable estimates of academic journals' prestige. The data is freely available at https://www.scimagojr.com
+# I wrote a minimalist package to have their data easily accessible in R: https://github.com/ikashnitsky/sjrdata 
+
+
+library(tidyverse)
+library(magrittr)
+# # if needed uncomment (crtl/cmd + SHIFT + C) & run
+# remotes::install_github("ikashnitsky/sjrdata") 
+library(sjrdata)
+
+# load the dataset
+df <- sjr_journals
+
+# I also added a more detailed Scopus categorization of scientific fields
+asjc <- readxl::read_xlsx("data/scopus-asjc-codes.xlsx")
+
+# Link to submit your output
+# https://bit.ly/bssd22-challenge
